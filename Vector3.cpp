619edde1579fdfb251131510c2413487bb7e48d1
陽人 加瀬 <0mh06559726484a@au.com>
2024-04-18 16:02:36 +0900

    三角形を描画して回転させる

--- Vector3.cpp
+++ Vector3.cpp
@@ -1,13 +1,18 @@
 ﻿#include"Vector3.h"
 
 Vector3 Vector3::operator*(const float& scalar) const {
-	return { x * scalar,y * scalar };
+	return { x * scalar,y * scalar ,z * scalar };
 }
 
 Vector3& Vector3::operator*=(float scalar) {
 	x *= scalar;
 	y *= scalar;
+	z *= scalar;
 	return *this;
+}
+
+Vector3 Vector3::operator/(const float& divisor) const {
+	return { x / divisor,y / divisor,z / divisor };
 }
 
 Vector3& Vector3::operator/=(float divisor) {
@@ -39,13 +44,3 @@ Vector3 Vector3::operator-=(const Vector3& other) {
 	return *this;
 }
 
-//ベクトルの乗算
-Vector3 Vector3::operator*(const Vector3& other) const {
-	return { x * other.x, y * other.y };
-}
-
-Vector3 Vector3::operator*=(const Vector3& other) {
-	this->x *= other.x;
-	this->y *= other.y;
-	return *this;
-}
