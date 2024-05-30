80fe325c4b67f289007bfa505c0c183b006d13fb
kase-haruto <0te06559726481t@au.com>
2024-05-30 09:02:32 +0900

    02_03

--- Collision.cpp
+++ Collision.cpp
@@ -1,4 +1,4 @@
-#include "Collision.h"
+﻿#include "Collision.h"
 #include"MyFunc.h"
 
 bool IsCollision(const Sphere* s, const Plane* plane){
@@ -17,4 +17,26 @@ bool IsCollision(const Sphere* s, const Plane* plane){
 		return true;
 	}
 	return false;
+}
+
+bool IsCollision(const Segment& segment, const Plane* plane){
+    float planeDis = plane->GetDistance();
+    Vector3 planeNormal = plane->GetNormal();
+    Vector3 n = Vector3::Normalize(planeNormal);
+    float d = Dot(n, segment.diff);
+
+    // 平行だから衝突しない
+    if (d == 0.0f){
+        return false;
+    }
+
+    // tを求める
+    float t = (planeDis - Dot(segment.origin, n)) / d;
+
+    // tが0から1の範囲にある場合、衝突している
+    if (t >= 0.0f && t <= 1.0f){
+        return true;
+    }
+
+    return false;
 }
